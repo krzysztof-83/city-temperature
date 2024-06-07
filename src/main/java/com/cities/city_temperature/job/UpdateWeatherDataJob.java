50a74f499cc601aa6f597fe838d3c24b77a7ceeb
kkawula <kkawula@oakfusion.pl>
2024-06-07 09:05:52 +0200

    refactor in scheduler

--- src/main/java/com/cities/city_temperature/job/UpdateWeatherDataJob.java
+++ src/main/java/com/cities/city_temperature/job/UpdateWeatherDataJob.java
@@ -3,15 +3,20 @@ package com.cities.city_temperature.job;
 
 import com.cities.city_temperature.service.CityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
+@Slf4j
 public class UpdateWeatherDataJob {
 
     private final CityService cityService;
 
     @Scheduled(cron = "0 0 12 * * *")
     public void updateWeatherData() {
-       cityService.updateTemperatures();
+        log.info("Updating weather data...");
+        cityService.updateTemperatures();
     }
 }
